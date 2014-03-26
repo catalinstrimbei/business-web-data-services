ec4e852cc253be3775cc202d6e1a3838530e45c6
catalin.strimbei <catalin.strimbei@gmail.com>
2014-03-26 21:25:12 +0000

    Scrum REST

--- SCRUM/ScrumREST/src/org/app/scrum/ejb/ScrumProjectDataServiceEJB.java
+++ SCRUM/ScrumREST/src/org/app/scrum/ejb/ScrumProjectDataServiceEJB.java
@@ -1,7 +1,8 @@
 package org.app.scrum.ejb;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -10,11 +11,11 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import org.app.patterns.EntityRepository;
 import org.app.patterns.EntityRepositoryBase;
 import org.app.patterns.ProjectBuilder;
-import org.app.scrum.Feature;
 import org.app.scrum.Project;
 import org.app.scrum.Release;
 import org.app.scrum.team.Team;
@@ -23,7 +24,7 @@ import org.app.scrum.team.Team;
  * Session Bean implementation class ScrumTeamRepositoryService
  * Aggregate Repository Service Facade: Project - features - releases
  */
-@Path("/scrum/")
+@Path("scrum")
 // 1. Remote interface
 @Stateless
 @LocalBean
@@ -49,7 +50,8 @@ public class ScrumProjectDataServiceEJB extends EntityRepositoryBase<Project> im
 	}
 
 	@GET
-	public String sayMessage() {
+	@Produces("text/html")
+	public String sayRest() {
 		return "Scrumming ";
 	}
 	
@@ -97,6 +99,8 @@ public class ScrumProjectDataServiceEJB extends EntityRepositoryBase<Project> im
 		}
 		return ((project != null) ? project.getReleases() : null) ;
 	}	
+	
+	
 }
 
 //  http://localhost:8080/HybridSample/resources/genericresource/samplemethod
