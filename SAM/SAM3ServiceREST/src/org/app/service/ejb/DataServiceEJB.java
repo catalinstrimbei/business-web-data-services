613ad60e0608c0ec9a5fbc86d44c4fa1ba98f1a3
catalin.strimbei <catalin.strimbei@gmail.com>
2014-03-27 11:08:32 +0000

    sprint 2

---
+++ SAM/SAM3ServiceREST/src/org/app/service/ejb/DataServiceEJB.java
@@ -0,0 +1,54 @@
+package org.app.service.ejb;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.app.patterns.EntityRepository;
+import org.app.patterns.EntityRepositoryBase;
+import org.app.service.entities.EntityBase;
+
+/**
+ * Session Bean implementation class ScrumTeamRepositoryService
+ * Aggregate Repository Service Facade: Project - features - releases
+ */
+@Path("service")
+// 1. Remote interface
+@Stateless
+@LocalBean
+public class DataServiceEJB extends EntityRepositoryBase<EntityBase> implements DataService{
+	private static Logger logger = Logger.getLogger(DataServiceEJB.class.getName());
+	
+	// 2. Inject resource 
+//	@PersistenceContext(unitName="ScrumEJB")
+//	private EntityManager scrumEM;
+
+    // 3. Init with injected EntityManager
+	private EntityRepository<EntityBase> teamRepository;
+	
+    @PostConstruct
+	public void init(){
+		logger.info("Initialized :");		
+	}	
+
+	public DataServiceEJB() {
+		super();
+		logger.info("INIT DEF CONSTRUCTOR ScrumProjectDataServiceEJB : " + this.em);		
+	}
+
+	
+	/********************************************************************/
+	@GET
+	@Produces("text/html")
+	public String sayRest() {
+		return "Rest-EJB-SAM3...";
+	}	
+	/********************************************************************/
+}
+
+// http://localhost:8080/ServiceREST/service
