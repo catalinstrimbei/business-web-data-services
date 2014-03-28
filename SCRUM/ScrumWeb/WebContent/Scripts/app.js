fb3efec457be31094c3b253f2a14fb2ffa6d5e54
catalin.strimbei <catalin.strimbei@gmail.com>
2014-03-28 11:44:55 +0000

    Start Scrum Web

---
+++ SCRUM/ScrumWeb/WebContent/Scripts/app.js
@@ -0,0 +1,8 @@
+function SimpleController($scope){
+	$scope.custms = [
+			{name: 'NS1', city: 'c1'}, 
+			{name: 'NS2', city: 'c2'}, 
+			{name: 'NS3', city: 'c3'}, 
+			{name: 'NS4', city: 'c4'}
+	];
+}
