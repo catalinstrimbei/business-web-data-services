caa585ec3885486b53684cf6ed280d9a792d33c5
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@b264670c-68ef-3839-4610-a8dc2f001232>
2014-03-29 14:23:44 +0000

    Table DAO added (demos)

--- trunk/SCRUM/ScrumREST/src/org/app/patterns/BaseTableDAO.java
+++ trunk/SCRUM/ScrumREST/src/org/app/patterns/BaseTableDAO.java
@@ -5,5 +5,6 @@ public class BaseTableDAO implements TableDAO{
 	// encapsulate JDBC Driver, Connection, Statement, Resultset logic
 	
 	// injected with JDBC DataSource ejb-container resource 
+	// simple
 
 }
