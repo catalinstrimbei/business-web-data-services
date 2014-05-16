574bb27b39473c96c7b404e075f8cd70b5eafc41
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@b264670c-68ef-3839-4610-a8dc2f001232>
2014-05-16 05:37:57 +0000

    AngularJS Start

--- trunk/SCRUM/ScrumWeb/WebContent/Scripts/start1.js
+++
@@ -1,8 +0,0 @@
-function SimpleController($scope){
-	$scope.custms = [
-			{name: 'NS1', city: 'c1'}, 
-			{name: 'NS2', city: 'c2'}, 
-			{name: 'NS3', city: 'c3'}, 
-			{name: 'NS4', city: 'c4'}
-	];
-}
