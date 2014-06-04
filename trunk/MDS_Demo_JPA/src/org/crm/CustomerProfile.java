fbc6d9adb84e18bfa79fc6b749cb810b5afcbfef
catalin.strimbei@gmail.com <catalin.strimbei@gmail.com@b264670c-68ef-3839-4610-a8dc2f001232>
2014-06-04 15:38:24 +0000

    JPA Integration Pro

---
+++ trunk/MDS_Demo_JPA/src/org/crm/CustomerProfile.java
@@ -0,0 +1,99 @@
+package org.crm;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity @Table(name="CUSTOMERS_PROFILES")
+public class CustomerProfile {
+	@Id @Column(name="customer_id") 
+	private Integer custId;
+	
+	private String name;
+	
+	private String region;
+	
+	private String address;
+	
+	private String country;
+	
+	@Column(name="custr_category")
+	private String customerCategory;
+
+	public Integer getCustId() {
+		return custId;
+	}
+
+	public void setCustId(Integer custId) {
+		this.custId = custId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCustomerCategory() {
+		return customerCategory;
+	}
+
+	public void setCustomerCategory(String customerCategory) {
+		this.customerCategory = customerCategory;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((custId == null) ? 0 : custId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CustomerProfile other = (CustomerProfile) obj;
+		if (custId == null) {
+			if (other.custId != null)
+				return false;
+		} else if (!custId.equals(other.custId))
+			return false;
+		return true;
+	}
+
+	
+	
+}
---
+++ trunk/MDS_Demo_JPA/src/org/erp/ProductNom.java
@@ -0,0 +1,68 @@
+package org.erp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity @Table(name="PRODUCT_NOM")
+public class ProductNom {
+	@Id @Column(name="product_id") 
+	private Integer productId;
+	
+	private String name;
+	
+	private String description;
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((productId == null) ? 0 : productId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductNom other = (ProductNom) obj;
+		if (productId == null) {
+			if (other.productId != null)
+				return false;
+		} else if (!productId.equals(other.productId))
+			return false;
+		return true;
+	}
+	
+	
+}
