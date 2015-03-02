02bc4b52a71d05b4e1e0f1ca3ee63bdb8f9b1720
catalin.strimbei <catalin.strimbei@gmail.com>
2015-03-02 15:10:33 +0000

    Added apache-common-math dependency.

---
+++ SCRUM/NumberGeneratorANT/src/org/tests/App.java
@@ -0,0 +1,36 @@
+package org.tests;
+ 
+import java.util.UUID;
+
+import org.apache.commons.math3.random.RandomDataGenerator;
+ 
+/**
+ * Generate a unique number
+ *
+ */
+public class App 
+{
+ 
+    public static void main( String[] args )
+    {
+        App obj = new App();
+        System.out.println("Unique ID : " + obj.generateUniqueKey());
+        System.out.println("Unique ID (2): " + obj.generateUniqueKey2());
+    }
+ 
+    public String generateUniqueKey(){
+ 
+    	String id = UUID.randomUUID().toString();
+    	return id;
+ 
+    }
+    /*
+     * http://commons.apache.org/proper/commons-math/userguide/random.html
+     */
+    public String generateUniqueKey2(){
+    	RandomDataGenerator randomData = new RandomDataGenerator(); 
+    	String id = String.valueOf(randomData.nextLong(1, 1000000));
+    	return id;
+ 
+    }
+}
