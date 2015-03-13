b1b9f2a2857b604f1a25e568ffa62d06ab1aeca5
catalin.strimbei <catalin.strimbei@gmail.com>
2015-03-13 18:08:31 +0000

    Refactoring/Cleaning 2015.

--- SCRUM/ScrumEJB/ejbModule/org/app/patterns/IProjectService.java
+++
@@ -1,9 +0,0 @@
-package org.app.patterns;
-
-import org.app.scrum.Project;
-
-public interface IProjectService extends Service{
-
-	public abstract Integer computeDaysSpentOnProject(Project project);
-
-}
--- SCRUM/ScrumEJB/ejbModule/org/app/scrum/sprint/TaskCategory.java
+++
@@ -1,7 +0,0 @@
-package org.app.scrum.sprint;
-
-// JPA cu @Enumerated
-
-public enum TaskCategory{
-	ANALYSIS, DESIGN, IMPLEMENTATION, TEST;
-}
