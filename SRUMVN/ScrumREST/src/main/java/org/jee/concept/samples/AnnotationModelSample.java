468f91df7af6dfca190e05e912ba46c56b932837
catalin.strimbei <catalin.strimbei@gmail.com>
2015-04-03 11:34:52 +0000

    AnnotationSample Start ...

---
+++ SRUMVN/ScrumREST/src/main/java/org/jee/concept/samples/AnnotationModelSample.java
@@ -0,0 +1,10 @@
+package org.jee.concept.samples;
+
+/*
+ * Simple demo to show:
+ * - how annotation work;
+ * - why annotation could be useful: adding simple label-based meta-data to classes 
+ */
+public class AnnotationModelSample {
+
+}
