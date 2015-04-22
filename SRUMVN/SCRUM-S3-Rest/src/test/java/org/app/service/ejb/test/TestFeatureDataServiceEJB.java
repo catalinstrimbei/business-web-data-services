e82fff66ab8ff391b18f65ab1675ec70ddd9c6da
catalin.strimbei <catalin.strimbei@gmail.com>
2015-04-22 12:59:28 +0000

    EJB_DataServices: FeatureDataService, ProjectDataService @FixMethodOrder to control JUnit test order

--- SRUMVN/SCRUM-S3-Rest/src/test/java/org/app/service/ejb/test/TestFeatureDataServiceEJB.java
+++ SRUMVN/SCRUM-S3-Rest/src/test/java/org/app/service/ejb/test/TestFeatureDataServiceEJB.java
@@ -5,13 +5,14 @@ import static org.junit.Assert.assertTrue;
 import java.util.Collection;
 import java.util.logging.Logger;
 
-import javax.ejb.EJB;
-
 import org.app.service.ejb.FeatureDataService;
 import org.app.service.entities.Feature;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestFeatureDataServiceEJB {
 	private static Logger logger = Logger.getLogger(TestFeatureDataServiceEJB.class.getName());
 	
@@ -21,25 +22,29 @@ public class TestFeatureDataServiceEJB {
 	public static void setUpBeforeClass() throws Exception {
 		service = FeatureDataServiceEJBFactory.getService();
 	}	
-	
+
 	@Test
-	public void testGetMessage() {
+	public void test0GetMessage() {
 		logger.info("DEBUG: Junit TESTING: testGetMessage ...");
 		
 //		String response = service.sayRest();
 //		assertNotNull("Data Service failed!", response);
 //		logger.info("DEBUG: EJB Response ..." + response);
 	}
-
+	
 	@Test
-	public void testGetFeatures() {
-		logger.info("DEBUG: Junit TESTING: testGetFeatures ...");
+	public void test1DeleteFeature() {
+		logger.info("DEBUG: Junit TESTING: testDeleteFeature ...");
+		
 		Collection<Feature> features = service.getFeatures();
-		assertTrue("Fail to read features!", features.size() > 0);
-	}
-
+		for (Feature f: features)
+			service.removeFeature(f);
+		Collection<Feature> featuresAfterDelete = service.getFeatures();
+		assertTrue("Fail to read features!", featuresAfterDelete.size() == 0);
+	}	
+	
 	@Test
-	public void testAddFeature() {
+	public void test2AddFeature() {
 		logger.info("DEBUG: Junit TESTING: testAddFeature ...");
 		
 		Integer featuresToAdd = 3;
@@ -49,16 +54,12 @@ public class TestFeatureDataServiceEJB {
 		}
 		Collection<Feature> features = service.getFeatures();
 		assertTrue("Fail to add features!", features.size() == featuresToAdd);
-	}
+	}	
 
 	@Test
-	public void testDeleteFeature() {
-		logger.info("DEBUG: Junit TESTING: testDeleteFeature ...");
-		
+	public void test3GetFeatures() {
+		logger.info("DEBUG: Junit TESTING: testGetFeatures ...");
 		Collection<Feature> features = service.getFeatures();
-		for (Feature f: features)
-			service.removeFeature(f);
-		Collection<Feature> featuresAfterDelete = service.getFeatures();
-		assertTrue("Fail to read features!", featuresAfterDelete.size() == 0);
+		assertTrue("Fail to read features!", features.size() > 0);
 	}	
 }
