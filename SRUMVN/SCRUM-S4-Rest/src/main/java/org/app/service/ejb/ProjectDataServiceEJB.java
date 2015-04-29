ba2ee104c538173d296237fa558721d01370f676
catalin.strimbei <catalin.strimbei@gmail.com>
2015-04-29 12:44:20 +0000

    Initial refactoring. [REST Data Service]

--- SRUMVN/SCRUM-S4-Rest/src/main/java/org/app/service/ejb/ProjectDataServiceEJB.java
+++ SRUMVN/SCRUM-S4-Rest/src/main/java/org/app/service/ejb/ProjectDataServiceEJB.java
@@ -1,6 +1,9 @@
 package org.app.service.ejb;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
@@ -20,12 +23,41 @@ import org.app.service.entities.Release;
 public class ProjectDataServiceEJB 
 		extends EntityRepositoryBase<Project>
 		implements ProjectDataService {
-	
 	private static Logger logger = Logger.getLogger(ProjectDataServiceEJB.class.getName());
-	
+			
+	@EJB // injected DataService
+	private FeatureDataService featureDataService; 
+	// Local component-entity-repository
+	private EntityRepository<Release> releaseRepository;
 	@PostConstruct
 	public void init() {
-		logger.info("POSTCONSTRUCT-INIT injected EntityManager: " + this.em);
+		// local instantiation of local component-entity-repository
+		releaseRepository = new EntityRepositoryBase<Release>(this.em,Release.class);
+		logger.info("POSTCONSTRUCT-INIT releaseRepository: " + this.releaseRepository);
+		logger.info("POSTCONSTRUCT-INIT featureDataService: " + this.featureDataService);
+	}
+	
+	// Aggregate factory method
+	public Project createNewProject(Integer id){
+		// create project aggregate
+		Project project = new Project(id, "NEW Project" + "." + id , new Date());
+		List<Release> releasesProject = new ArrayList<>();
+		Date dataPublicare = new Date();
+		Long interval =  30l /*zile*/ * 24 /*ore*/ * 60 /*min*/ * 60 /*sec*/ * 1000 /*milisec*/;
+		Integer releaseCount = 3;
+		for (int i=0; i<=releaseCount-1; i++){
+			releasesProject.add(new Release(null, "R: " + project.getProjectNo() + "." + i, 
+					new Date(dataPublicare.getTime() + i * interval), project));
+		}
+		project.setReleases(releasesProject);		
+		// save project aggregate
+		this.add(project);
+		// return project aggregate to service client
+		return project;
+	}
+	
+	public Release getReleaseById(Integer releaseid) {
+		return releaseRepository.getById(releaseid);
 	}
 
 	public String getMessage() {
--- SRUMVN/SCRUM-S4-Rest/src/test/java/org/app/service/ejb/test/TestProjectDataServiceEJBArq.java
+++ SRUMVN/SCRUM-S4-Rest/src/test/java/org/app/service/ejb/test/TestProjectDataServiceEJBArq.java
@@ -1,18 +1,20 @@
 package org.app.service.ejb.test;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.ejb.EJB;
 
 import org.app.patterns.EntityRepository;
-import org.app.patterns.EntityRepositoryBase;
+import org.app.service.ejb.FeatureDataService;
+import org.app.service.ejb.FeatureDataServiceEJB;
 import org.app.service.ejb.ProjectDataService;
 import org.app.service.ejb.ProjectDataServiceEJB;
 import org.app.service.entities.Project;
+import org.app.service.entities.Release;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
@@ -24,27 +26,67 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
-@RunWith(Arquillian.class) 
+@RunWith(Arquillian.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestProjectDataServiceEJBArq {
-	private static Logger logger = Logger.getLogger(TestProjectDataServiceEJBArq.class.getName());
-	
-	@EJB
-	private static ProjectDataService service;
-	
-	@Deployment // Arquilian infrastructure
+	private static Logger logger = Logger
+			.getLogger(TestProjectDataServiceEJBArq.class.getName());
+
+	// Arquilian infrastructure
+	@Deployment
 	public static Archive<?> createDeployment() {
-	        return ShrinkWrap
-	                .create(WebArchive.class, "SCRUM-S4-test.war")
-	                .addPackage(Project.class.getPackage())
-	                .addClass(ProjectDataService.class)
-	                .addClass(ProjectDataServiceEJB.class)
-	                .addClass(EntityRepository.class)
-	                .addClass(EntityRepositoryBase.class)
-	                .addAsResource("META-INF/persistence.xml")
-	                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-	}	
-	
+		return ShrinkWrap.create(WebArchive.class, "SCRUM-S4-test.war")
+				.addPackage(EntityRepository.class.getPackage())
+				.addPackage(Project.class.getPackage())
+				.addClass(FeatureDataService.class)
+				.addClass(FeatureDataServiceEJB.class)
+				.addClass(ProjectDataService.class)
+				.addClass(ProjectDataServiceEJB.class)
+				.addAsResource("META-INF/persistence.xml")
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@EJB
+	// Test EJB Data Service Reference is injected
+	private static ProjectDataService service;
+
+	// JUnit test methods
+	@Test
+	public void test4_GetProject() {
+		logger.info("DEBUG: Junit TESTING: testGetProject 7002 ...");
+		Project project = service.getById(7002);
+		assertNotNull("Fail to Get Project 7002!", project);
+	}
+
+	/* CREATE Test 2: create aggregate */
+	@Test
+	public void test3_CreateNewProject() {
+		Project project = service.createNewProject(7002);
+		assertNotNull("Fail to create new project in repository!", project);
+		// update project
+		project.setName(project.getName() + " - changed by test client");
+		List<Release> releases = project.getReleases();
+		for (Release r : releases)
+			r.setIndicative(r.getIndicative() + " - changed by test client");
+		project = service.add(project);
+		assertNotNull("Fail to save new project in repository!", project);
+		logger.info("DEBUG createNewProject: project changed: " + project);
+		// check read
+		project = service.getById(7002); // !!!
+		assertNotNull("Fail to find changed project in repository!", project);
+		logger.info("DEBUG createNewProject: queried project" + project);
+	}
+
+	@Test
+	public void test2_DeleteProject() {
+		logger.info("DEBUG: Junit TESTING: testDeleteProject 7002...");
+		Project project = service.getById(7002); // !!!
+		if (project != null)
+			service.remove(project);
+		project = service.getById(7002); // !!!
+		assertNull("Fail to delete Project 7002!", project);
+	}
+
 	@Test
 	public void test1_GetMessage() {
 		logger.info("DEBUG: Junit TESTING: testGetMessage ...");
@@ -52,34 +94,4 @@ public class TestProjectDataServiceEJBArq {
 		assertNotNull("Data Service failed!", response);
 		logger.info("DEBUG: EJB Response ..." + response);
 	}
-
-	@Test
-	public void test4_GetProjects() {
-		logger.info("DEBUG: Junit TESTING: testGetProjects ...");
-		Collection<Project> Projects = service.toCollection();
-		assertTrue("Fail to read Projects!", Projects.size() > 0);
-	}
-
-	@Test
-	public void test3_AddProject() {
-		logger.info("DEBUG: Junit TESTING: testAddProject ...");
-		
-		Integer projectsToAdd = 3;
-		for (int i=1; i <= projectsToAdd; i++){
-			service.add(new Project(i, "Project_" + (100 + i)));
-		}
-		Collection<Project> projects = service.toCollection();
-		assertTrue("Fail to add Projects!", projects.size() == projectsToAdd);
-	}
-
-	@Test
-	public void test2_DeleteProject() {
-		logger.info("DEBUG: Junit TESTING: testDeleteProject ...");
-		
-		Collection<Project> projects = service.toCollection();
-		for (Project p: projects)
-			service.remove(p);
-		Collection<Project> ProjectsAfterDelete = service.toCollection();
-		assertTrue("Fail to read Projects!", ProjectsAfterDelete.size() == 0);
-	}	
 }
