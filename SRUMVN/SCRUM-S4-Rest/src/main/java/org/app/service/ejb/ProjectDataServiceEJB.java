cd10d18898f053766c12037b21fcb12271b0cc8d
catalin.strimbei <catalin.strimbei@gmail.com>
2015-05-05 10:35:05 +0000

    REST Services: FeatureDataService, ProjectDataService

--- SRUMVN/SCRUM-S4-Rest/src/main/java/org/app/service/ejb/ProjectDataServiceEJB.java
+++ SRUMVN/SCRUM-S4-Rest/src/main/java/org/app/service/ejb/ProjectDataServiceEJB.java
@@ -10,7 +10,13 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -40,6 +46,76 @@ public class ProjectDataServiceEJB
 		logger.info("POSTCONSTRUCT-INIT featureDataService: " + this.featureDataService);
 	}
 	
+	/******** REST MAPPING IMPLEMENTATION ************************************************/
+
+	@Override
+	@GET 					/* scrum/data/projects 		REST-resource: projects-collection*/
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Collection<Project> toCollection() {
+		logger.info("**** DEBUG REST toCollection()");
+		return super.toCollection();
+	}	
+	
+	
+	@GET @Path("/{id}") 	/* scrum/data/projects/data/{id} 	REST-resource: project-entity*/
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })	
+	public Project getById(@PathParam("id") Integer id){ 
+		Project project = super.getById(id);
+		logger.info("**** DEBUG REST getById(" + id +") = " + project);
+		return project;
+	}	
+	
+	@POST 					/* scrum/data/projects 		REST-resource: projects-collection*/
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })	
+	public Collection<Project> addNew(Project project) {
+		super.add(project);
+		return Project.toDTOs(super.toCollection());
+	}
+	
+	@PUT @Path("/{id}") 	/* scrum/data/projects/{id} 	REST-resource: project-entity*/	
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })	
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW) // autonomous transaction
+	@Override
+	public Project add(Project project) {
+		project = super.add(project);
+		return project;
+		//return Project.toDTOAggregate(project);
+	}	
+	
+	@Override
+	@DELETE 				/* scrum/data/projects 		REST-resource: projects-collection*/
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })	
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW) // autonomous transaction
+	public boolean remove(Project project) {
+		logger.info("DEBUG: called REMOVE - project: " + project);
+		return super.remove(project);
+	}
+	
+	@DELETE @Path("/{id}") 	/* scrum/data/projects/{id} 	REST-resource: project-entity*/	
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW) // autonomous transaction
+	public void remove(@PathParam("id")Integer id) {
+		logger.info("DEBUG: called REMOVE - ById() for projects >>>>>>>>>>>>>> simplified ! + id");
+		Project project = super.getById(id);
+		super.remove(project);
+	}
+	
+	// GET method on second repository for Release-type entities
+	@GET @Path("/{projectid}/releases/{releaseid}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+	public Release getReleaseById(@PathParam("releaseid") Integer releaseid){
+		logger.info("DEBUG: called getReleaseById() for projects >>>>>>>>>>>>>> simplified !");
+		Release release = releaseRepository.getById(releaseid);
+		return release;
+		// return release.toDTO();
+	}
+	
+	/* Other test-proposal methods ************************************************************/
+	@GET @Path("/new/{id}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })	
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW) // autonomous transaction
 	// Aggregate factory method
 	public Project createNewProject(Integer id){
 		// create project aggregate
@@ -59,31 +135,9 @@ public class ProjectDataServiceEJB
 		return project;
 	}
 	
-	public Release getReleaseById(Integer releaseid) {
-		return releaseRepository.getById(releaseid);
-	}
-
-	public String getMessage() {
+	@GET @Path("/test") // Check if resource is up ...
+	@Produces({ MediaType.TEXT_PLAIN})
+	public String getMessage(){
 		return "ProjectSprint DataService is working...";
-	}
-
-	@Override
-	@GET 					/* scrum/data/projects 		REST-resource: projects-collection*/
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-//	@Produces({ MediaType.APPLICATION_JSON })
-	public Collection<Project> toCollection() {
-		logger.info("**** DEBUG REST toCollection()");
-		return super.toCollection();
-		//return Project.toDTOs(super.toCollection());
-	}	
-	
-	
-	@GET @Path("/{id}") 	/* scrum/projects/data/{id} 	REST-resource: project-entity*/
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })	
-	public Project getById(@PathParam("id") Integer id){ 
-		Project project = super.getById(id);
-		logger.info("**** DEBUG REST getById(" + id +") = " + project);
-		return project;
-//		return Project.toDTOAggregate(project);
 	}	
 }
